2c9ee2d701df5ef619676a7a79636b4a78250c52
dristic <dristic@book.com>
2012-10-22 11:41:05 -0700

    Add uk support file.

---
+++ languages/uk.js
@@ -0,0 +1,34 @@
+// numeral.js language configuration
+// language : united kingdom (uk)
+// author : Adam Draper : https://github.com/adamwdraper
+(function () {
+    var language = {
+        delimiters: {
+            thousands: ',',
+            decimal: '.'
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'm'
+        },
+        ordinal: function (number) {
+            var b = number % 10;
+            return (~~ (number % 100 / 10) === 1) ? 'th' :
+                (b === 1) ? 'st' :
+                (b === 2) ? 'nd' :
+                (b === 3) ? 'rd' : 'th';
+        },
+        money: {
+            symbol: '£'
+        }
+    };
+
+    // Node
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
+        this.numeral.language('uk', language);
+    }
+}());
