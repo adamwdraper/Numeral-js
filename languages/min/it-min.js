1577f22eb5ad5a4ed43911611a515ae06b592ef2
Anatoli Papirovski <apapirovski@mac.com>
2012-10-31 19:53:35 -0400

    Failed to update the minifed file and the tests to account for ordinal formatting change

--- languages/min/it-min.js
+++ languages/min/it-min.js
@@ -1,4 +1,4 @@
 // numeral.js language configuration
 // language : italian Italy (it)
 // author : Giacomo Trombi : http://cinquepunti.it
-(function(){var e={delimiters:{thousands:".",decimal:","},abbreviations:{thousand:"mila",million:"mil"},ordinal:function(e){return"\u00b0"},currency:{symbol:"\u20ac"}};typeof module!="undefined"&&module.exports&&(module.exports=e),typeof window!="undefined"&&this.numeral&&this.numeral.language&&this.numeral.language("it",e)})();
+(function(){var e={delimiters:{thousands:".",decimal:","},abbreviations:{thousand:"mila",million:"mil"},ordinal:function(e){return"\u00ba"},currency:{symbol:"\u20ac"}};typeof module!="undefined"&&module.exports&&(module.exports=e),typeof window!="undefined"&&this.numeral&&this.numeral.language&&this.numeral.language("it",e)})();
