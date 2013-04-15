87f679983acabb0c403c4d723a7bd04906aad0d2
Adam Draper <adamwdraper@gmail.com>
2013-04-15 16:09:26 -0700

    add grunt uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,40 @@
+var fs = require('fs');
+
 module.exports = function(grunt) {
+
+    var minifiedFiles = {
+            'min/numeral.min.js' : ['numeral.js']
+        };
+
+    // all the lang files need to be added manually
+    fs.readdirSync('./languages').forEach(function (path) {
+        if (path.indexOf('.js') > -1) {
+            var destination = 'min/languages/' + path,
+                src = ['languages/' + path];
+
+            minifiedFiles[destination] = src;
+        }
+    });
+
     grunt.initConfig({
         qunit: {
             src: ['tests/**/*.html']
+        },
+        uglify : {
+            my_target: {
+                files: minifiedFiles
+            },
+            options: {
+                preserveComments: 'some'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-qunit');
-    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
     grunt.registerTask('test', 'qunit:src');
 
+    // Travis CI task.
     grunt.registerTask('travis', ['test']);
 };
