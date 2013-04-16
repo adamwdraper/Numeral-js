6ad2b66f40ce8ba7f3ca6148deac136206bc5d69
Adam Draper <adamwdraper@gmail.com>
2013-04-15 20:41:00 -0700

    minify be-nl language

--- min/languages/be-nl.js
+++ min/languages/be-nl.js
@@ -3,4 +3,4 @@
  * language : belgium-dutch (be-nl)
  * author : Dieter Luypaert : https://github.com/moeriki
  */
-(function(){var n={delimiters:{thousands:" ",decimal:","},abbreviations:{thousand:"k",million:"m",billion:"b",trillion:"t"},ordinal:function(n){return 1===n||n>=20?"ste":"de"},currency:{symbol:"€"}};"undefined"!=typeof module&&module.exports&&(module.exports=n),"undefined"!=typeof window&&this.numeral&&this.numeral.language&&this.numeral.language("be-nl",n)})();
+(function(){var n={delimiters:{thousands:" ",decimal:","},abbreviations:{thousand:"k",million:" mln",billion:" mld",trillion:" bln"},ordinal:function(n){var e=n%100;return 0!==n&&1>=e||8===e||e>=20?"ste":"de"},currency:{symbol:"€ "}};"undefined"!=typeof module&&module.exports&&(module.exports=n),"undefined"!=typeof window&&this.numeral&&this.numeral.language&&this.numeral.language("be-nl",n)})();
