054a6b9b4283633a39fdf31678f3ce81b4b6c866
Adam Draper <adamwdraper@gmail.com>
2013-04-18 21:52:37 -0700

    Minify new languages

---
+++ min/languages/ru-UA.js
@@ -0,0 +1,1 @@
+(function(){var n={delimiters:{thousands:" ",decimal:","},abbreviations:{thousand:"тыс.",million:"млн",billion:"b",trillion:"t"},ordinal:function(){return"."},currency:{symbol:"₴"}};"undefined"!=typeof module&&module.exports&&(module.exports=n),"undefined"!=typeof window&&this.numeral&&this.numeral.language&&this.numeral.language("ru-UA",n)})();
---
+++ min/languages/uk-UA.js
@@ -0,0 +1,1 @@
+(function(){var n={delimiters:{thousands:" ",decimal:","},abbreviations:{thousand:"тис.",million:"млн",billion:"млрд",trillion:"блн"},ordinal:function(){return""},currency:{symbol:"₴"}};"undefined"!=typeof module&&module.exports&&(module.exports=n),"undefined"!=typeof window&&this.numeral&&this.numeral.language&&this.numeral.language("uk-UA",n)})();
