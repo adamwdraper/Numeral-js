86715fcaec3d441cf4a80aafc3fdf9dc3b4a34e3
Adam Draper <adamwdraper@gmail.com>
2013-12-25 14:16:24 -0700

    format to match indention of other code

--- numeral.js
+++ numeral.js
@@ -480,41 +480,47 @@
      * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce#Compatibility
      */
     if ('function' !== typeof Array.prototype.reduce) {
-      Array.prototype.reduce = function(callback, opt_initialValue){
-        'use strict';
-        if (null === this || 'undefined' === typeof this) {
-          // At the moment all modern browsers, that support strict mode, have
-          // native implementation of Array.prototype.reduce. For instance, IE8
-          // does not support strict mode, so this check is actually useless.
-          throw new TypeError(
-              'Array.prototype.reduce called on null or undefined');
-        }
-        if ('function' !== typeof callback) {
-          throw new TypeError(callback + ' is not a function');
-        }
-        var index, value,
-            length = this.length >>> 0,
-            isValueSet = false;
-        if (1 < arguments.length) {
-          value = opt_initialValue;
-          isValueSet = true;
-        }
-        for (index = 0; length > index; ++index) {
-          if (this.hasOwnProperty(index)) {
-            if (isValueSet) {
-              value = callback(value, this[index], index, this);
+        Array.prototype.reduce = function (callback, opt_initialValue) {
+            'use strict';
+            
+            if (null === this || 'undefined' === typeof this) {
+                // At the moment all modern browsers, that support strict mode, have
+                // native implementation of Array.prototype.reduce. For instance, IE8
+                // does not support strict mode, so this check is actually useless.
+                throw new TypeError('Array.prototype.reduce called on null or undefined');
             }
-            else {
-              value = this[index];
-              isValueSet = true;
+            
+            if ('function' !== typeof callback) {
+                throw new TypeError(callback + ' is not a function');
             }
-          }
-        }
-        if (!isValueSet) {
-          throw new TypeError('Reduce of empty array with no initial value');
-        }
-        return value;
-      };
+
+            var index,
+                value,
+                length = this.length >>> 0,
+                isValueSet = false;
+
+            if (1 < arguments.length) {
+                value = opt_initialValue;
+                isValueSet = true;
+            }
+
+            for (index = 0; length > index; ++index) {
+                if (this.hasOwnProperty(index)) {
+                    if (isValueSet) {
+                        value = callback(value, this[index], index, this);
+                    } else {
+                        value = this[index];
+                        isValueSet = true;
+                    }
+                }
+            }
+
+            if (!isValueSet) {
+                throw new TypeError('Reduce of empty array with no initial value');
+            }
+
+            return value;
+        };
     }
 
     
