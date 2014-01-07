cea66c0a9cc414ee9bf5877d51aa517641d5b555
Ove Andersen <ove.andersen@outlook.com>
2014-01-07 15:44:36 +0100

    Added norwegian translation

---
+++ languages/nb-no.js
@@ -0,0 +1,34 @@
+/*! 
+ * numeral.js language configuration
+ * language : norwegian (bokmål)
+ * author : Ove Andersen : https://github.com/azzlack
+ */
+(function () {
+    var language = {
+        delimiters: {
+            thousands: ' ',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'm',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function (number) {
+            return '.';
+        },
+        currency: {
+            symbol: 'kr'
+        }
+    };
+
+    // Node
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
+        this.numeral.language('nb-no', language);
+    }
+}());
