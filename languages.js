df46fe3075789b6c7f00b34adb36ec951a918697
Ove Andersen <ove.andersen@outlook.com>
2014-01-07 15:45:29 +0100

    Build

--- languages.js
+++ languages.js
@@ -604,6 +604,40 @@
 
 /*! 
  * numeral.js language configuration
+ * language : norwegian (bokmål)
+ * author : Ove Andersen : https://github.com/azzlack
+ */
+(function () {
+    var language = {
+        delimiters: {
+            thousands: ' ',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'm',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function (number) {
+            return '.';
+        },
+        currency: {
+            symbol: 'kr'
+        }
+    };
+
+    // Node
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
+        this.numeral.language('nb-no', language);
+    }
+}());
+/*! 
+ * numeral.js language configuration
  * language : netherlands-dutch (nl-nl)
  * author : Dave Clayton : https://github.com/davedx
  */
