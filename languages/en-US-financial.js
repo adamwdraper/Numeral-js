58b07a0ba794034939873e10be69c75f95c586c9
Justin Collum <jcollum@hcgprevision.com>
2014-01-31 14:16:22 -0800

    added en-US-financial and en-US-metric languages

---
+++ languages/en-US-financial.js
@@ -0,0 +1,38 @@
+/*! 
+ * numeral.js language configuration
+ * language : English, US, metric suffixes
+ * author : Justin Collum : https://github.com/jcollum
+ */
+(function () {
+    var language = {
+        delimiters: {
+            thousands: ',',
+            decimal: '.'
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'MM',
+            billion: 'BN',
+            trillion: 'T'
+        },
+        ordinal: function (number) {
+            var b = number % 10;
+            return (~~ (number % 100 / 10) === 1) ? 'th' :
+                (b === 1) ? 'st' :
+                (b === 2) ? 'nd' :
+                (b === 3) ? 'rd' : 'th';
+        },
+        currency: {
+            symbol: '$'
+        }
+    };
+
+    // Node
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
+        this.numeral.language('en-US-financial', language);
+    }
+}());
