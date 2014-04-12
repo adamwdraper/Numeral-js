6389252a5c93753f9544a1ca2167e5fd1d5bc88c
Honza <Honza@Honza-PC>
2014-04-12 15:12:41 +0200

    Correct Czech translation with tests
    - Fix abbreviations for numerical orders
    - Do thousands separator right with non-breaking space

--- languages/cs.js
+++ languages/cs.js
@@ -6,14 +6,14 @@
 (function () {
     var language = {
         delimiters: {
-            thousands: ' ',
+            thousands: '\u00A0',
             decimal: ','
         },
         abbreviations: {
             thousand: 'tis.',
             million: 'mil.',
-            billion: 'b',
-            trillion: 't'
+            billion: 'mld.',
+            trillion: 'bil.'
         },
         ordinal: function () {
             return '.';
