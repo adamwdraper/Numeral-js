3c328c27409caaa135cd9df285139fd2c8c6a7c5
Divyanshu Arora <darora@palantir.com>
2014-08-22 14:11:25 +0900

    Remove millions, which is an odd abbreviation and very uncommon in Japanese

--- languages/ja.js
+++ languages/ja.js
@@ -12,7 +12,6 @@
         abbreviations: [
             { limits: [Math.pow(10, 3), Math.pow(10, 4)], string: '千'}, // thousands
             { limits: [Math.pow(10, 4), Math.pow(10, 6)], string: '万'}, // ten thousands
-            { limits: [Math.pow(10, 6), Math.pow(10, 8)], string: '百万'}, // million
             { limits: [Math.pow(10, 8), Math.pow(10, 9)], string: '億'}, // hundred million
             { limits: [Math.pow(10, 12), Number.POSITIVE_INFINITY], string: '兆'} // trillion
         ],
