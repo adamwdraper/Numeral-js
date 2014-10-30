9379c2ebd26687d29effca3dbabb328213764873
Sandstrom <mail@a16m.se>
2014-10-30 18:45:03 +0100

    Swedish (with tests)

---
+++ languages/sv.js
@@ -0,0 +1,34 @@
+/*!
+ * numeral.js language configuration
+ * language : Swedish
+ * author : Sandstrom : https://github.com/sandstrom
+ */
+(function () {
+    var language = {
+        delimiters: {
+            thousands: ' ',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'M',
+            billion: 'G',
+            trillion: 'T'
+        },
+        ordinal: function (number) {
+            return '.';
+        },
+        currency: {
+            symbol: 'kr'
+        }
+    };
+
+    // Node
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
+        this.numeral.language('sv', language);
+    }
+}());
