5797a4b765fc0c8609fe7e0097c295515e42758b
Aitor Moreno <aitor@rojo2.com>
2014-12-15 01:27:33 +0100

    Added test to verify that locale() loads locale file properly

--- tests/numeral/misc.js
+++ tests/numeral/misc.js
@@ -119,5 +119,38 @@ exports.misc = {
         test.strictEqual(formatTestVal(), oldCurrencyVal, 'Format uses old currency');
 
         test.done();
+    },
+
+    locale: function(test) {
+      numeral.locale('es');
+
+      test.expect(16);
+
+      var tests = [
+          [10000,'0,0.0000','10.000,0000'],
+          [10000.23,'0,0','10.000'],
+          [-10000,'0,0.0','-10.000,0'],
+          [10000.1234,'0.000','10000,123'],
+          [-10000,'(0,0.0000)','(10.000,0000)'],
+          [-0.23,'.00','-,23'],
+          [-0.23,'(.00)','(,23)'],
+          [0.23,'0.00000','0,23000'],
+          [1230974,'0.0a','1,2mm'],
+          [1460,'0a','1k'],
+          [-104000,'0a','-104k'],
+          [1,'0o','1er'],
+          [52,'0o','52do'],
+          [23,'0o','23er'],
+          [100,'0o','100mo'],
+          [1,'0[.]0','1']
+      ];
+
+      for (var i = 0; i < tests.length; i++) {
+          test.strictEqual(numeral(tests[i][0]).format(tests[i][1]), tests[i][2], tests[i][1]);
+      }
+
+      numeral.locale('en');
+
+      test.done();
     }
 };
