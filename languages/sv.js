e9397e0d5c8e8955b60a4014e5aa9589d87955fd
Alex Schenkman <alex@schenkman.info>
2015-02-11 21:56:32 +0100

    Added Swedish language.

---
+++ languages/sv.js
@@ -0,0 +1,35 @@
+/*! 
+ * numeral.js language configuration
+ * language : swedish
+ * author : Alex Schenkman : https://github.com/alesch
+ */
+(function () {
+    var language = {
+        delimiters: {
+            thousands: '.',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'm',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function (number) {
+            var b = number % 10;
+            return (b === 1 || b === 2) ? ':a' : ':e';
+        },
+        currency: {
+            symbol: 'kr'
+        }
+    };
+
+    // Node
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
+        this.numeral.language('sv', language);
+    }
+}());
