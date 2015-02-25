7bf0a16e213129d6cd578a4841614a5b0edda668
Jake Hiller <jakemhiller@gmail.com>
2015-02-25 13:08:22 -0500

    build

--- languages.js
+++ languages.js
@@ -33,41 +33,6 @@
         this.numeral.language('be-nl', language);
     }
 }());
-/*! 
- * numeral.js language configuration
- * language : simplified chinese
- * author : badplum : https://github.com/badplum
- */
-(function () {
-    var language = {
-        delimiters: {
-            thousands: ',',
-            decimal: '.'
-        },
-        abbreviations: {
-            thousand: '千',
-            million: '百万',
-            billion: '十亿',
-            trillion: '兆'
-        },
-        ordinal: function (number) {
-            return '.';
-        },
-        currency: {
-            symbol: '¥'
-        }
-    };
-
-    // Node
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = language;
-    }
-    // Browser
-    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
-        this.numeral.language('chs', language);
-    }
-}());
-
 /*!
  * numeral.js language configuration
  * language : czech (cs)
@@ -1019,5 +984,40 @@
     // Browser
     if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
         this.numeral.language('uk-UA', language);
+    }
+}());
+
+/*!
+ * numeral.js language configuration
+ * language : simplified chinese
+ * author : badplum : https://github.com/badplum
+ */
+(function () {
+    var language = {
+        delimiters: {
+            thousands: ',',
+            decimal: '.'
+        },
+        abbreviations: {
+            thousand: '千',
+            million: '百万',
+            billion: '十亿',
+            trillion: '兆'
+        },
+        ordinal: function (number) {
+            return '.';
+        },
+        currency: {
+            symbol: '¥'
+        }
+    };
+
+    // Node
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
+        this.numeral.language('zh', language);
     }
 }());
