60fb386818bc8304baef3d95f6984c886fa88329
Stewart Scott <stewart42@gmail.com>
2015-06-08 17:25:15 +0200

    build - include en-ZA

--- languages.js
+++ languages.js
@@ -205,6 +205,44 @@
         this.numeral.language('de', language);
     }
 }());
+/*!
+ * numeral.js language configuration
+ * language : English for South Africa
+ * author : Stewart Scott : https://github.com/stewart42
+ */
+(function() {
+    var language = {
+        delimiters: {
+            thousands: ',',
+            decimal: '.'
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'm',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function(number) {
+            var b = number % 10;
+            return (~~(number % 100 / 10) === 1) ? 'th' :
+                (b === 1) ? 'st' :
+                (b === 2) ? 'nd' :
+                (b === 3) ? 'rd' : 'th';
+        },
+        currency: {
+            symbol: 'R'
+        }
+    };
+
+    // Node
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
+        this.numeral.language('en-ZA', language);
+    }
+}());
 /*! 
  * numeral.js language configuration
  * language : english united kingdom (uk)
