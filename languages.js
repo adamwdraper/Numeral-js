7c5ba489f055eb6939d3947b7e5ca11a141a7127
Fajar Maulana Firdaus <fajarmf@salestock.id>
2015-06-11 13:56:45 +0700

    Add Bahasa Indonesia

--- languages.js
+++ languages.js
@@ -535,6 +535,40 @@
 }());
 /*! 
  * numeral.js language configuration
+ * language : Bahasa Indonesia (id)
+ * author : Fajar M Firdaus : http://fajarmf.net
+ */
+(function () {
+    var language = {
+        delimiters: {
+            thousands: '.',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'rb',
+            million: 'jt',
+            billion: 'm',
+            trillion: 't'
+        },
+        ordinal: function (number) {
+            return '.';
+        },
+        currency: {
+            symbol: 'Rp'
+        }
+    };
+
+    // Node
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
+        this.numeral.language('id', language);
+    }
+}());
+/*! 
+ * numeral.js language configuration
  * language : italian Italy (it)
  * author : Giacomo Trombi : http://cinquepunti.it
  */
