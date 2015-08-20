9f83c9cc303c073434e51a0ca5549f3fe6bdd9ca
Suki Media <media@suki.ws>
2015-08-20 17:01:49 +0200

    Fix ordinal numbers es-ES

--- languages/es-ES.js
+++ languages/es-ES.js
@@ -2,6 +2,7 @@
  * numeral.js language configuration
  * language : spanish Spain
  * author : Hernan Garcia : https://github.com/hgarcia
+            Jaume sala : https://github.com/jaumesala
  */
 (function () {
     var language = {
@@ -16,12 +17,7 @@
             trillion: 't'
         },
         ordinal: function (number) {
-            var b = number % 10;
-            return (b === 1 || b === 3) ? 'er' :
-                (b === 2) ? 'do' :
-                    (b === 7 || b === 0) ? 'mo' :
-                        (b === 8) ? 'vo' :
-                            (b === 9) ? 'no' : 'to';
+            return 'º';
         },
         currency: {
             symbol: '€'
