8a786ce63d8d74b06115a8b83e8ea84acd4cec39
lukefly <lukefly@gmail.com>
2015-09-03 15:52:25 +0900

    Update ko.js

--- languages/ko.js
+++ languages/ko.js
@@ -33,6 +33,6 @@
     }
     // Browser
     if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
-        this.numeral.language('en-gb', language);
+        this.numeral.language('ko', language);
     }
 }());
