c1d1b87d66bafdbb9a951a095cc934652bd2cb12
Niyoko Yuliawan <niyoko@niyoko.net>
2015-11-20 21:13:48 +0700

    Indonesian language Note: Ordinal tests will fail because in Indonesian, ordinals are written like this 'ke-3'

---
+++ languages/id-ID.js
@@ -0,0 +1,36 @@
+/*! 
+ * numeral.js language configuration
+ * language : Indonesian (ID)
+ * author : Niyoko Yuliawan (niyoko@niyoko.net)
+ */
+(function () {
+    var language = {
+        delimiters: {
+            thousands: '.',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'rb',
+            million: 'jt',
+            billion: 'my',
+            trillion: 'tr'
+        },
+        ordinal: function (number) {
+			// In Indonesian, ordinal is written as 'ke-' at the front of the number
+			// So, '3rd' is translated as 'ke-3'
+            return '';
+        },
+        currency: {
+            symbol: 'Rp'
+        }
+    };
+
+    // Node
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
+        this.numeral.language('id-ID', language);
+    }
+}());
