52e69ee9efc90390c67b4c3a56f488661761af2c
ali abdollahzade <aliabdzad1@gmail.com>
2015-11-26 12:58:51 +0330

    Update languages.js
    add fa-ir language

--- languages.js
+++ languages.js
@@ -363,6 +363,40 @@
 
 /*! 
  * numeral.js language configuration
+ * language : persian iran (fa-ir)
+ * author : Ali Abdollahzade : https://github.com/aliabdzad
+ */
+(function () {
+    var language = {
+        delimiters: {
+            thousands: ',',
+            decimal: '.'
+        },
+        abbreviations: {
+            thousand: 'هزار',
+            million: 'میلیون',
+            billion: 'میلیارد',
+            trillion: 'تریلیون'
+        },
+        ordinal: function (number) {
+            return 'ام';
+        },
+        currency: {
+            symbol: ' ﷼'
+        }
+    };
+
+    // Node
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
+        this.numeral.language('fa-ir', language);
+    }
+}());
+/*! 
+ * numeral.js language configuration
  * language : Finnish
  * author : Sami Saada : https://github.com/samitheberber
  */
