f5ce3c42552b95d7197e979959e4addfc1170dab
Manuel Darveau <manueldarveau@gmail.com>
2016-07-19 23:34:13 -0400

    Create fr-CA-fem.js
    In French, the ordinal suffix is not the same depending on the "gender" of the word. Such concept does not exist in English... at least not anymore according to Wikipedia!.

---
+++ languages/fr-CA-fem.js
@@ -0,0 +1,35 @@
+/*!
+ * numeral.js language configuration
+ * language : french (Canada) (fr-CA)
+ * author : Manuel Darveau : https://github.com/mdarveau
+ * Derived from work by Renaud-Allaire : https://github.com/renaudleo
+ */
+(function () {
+    var language = {
+        delimiters: {
+            thousands: ' ',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'M',
+            billion: 'G',
+            trillion: 'T'
+        },
+        ordinal : function (number) {
+            return number === 1 ? 'ère' : 'ème';
+        },
+        currency: {
+            symbol: '$'
+        }
+    };
+
+    // Node
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = language;
+    }
+    // Browser
+    if (typeof window !== 'undefined' && this.numeral && this.numeral.language) {
+        this.numeral.language('fr-CA', language);
+    }
+}());
