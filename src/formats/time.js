8aeab9a9d13ba4c55c52407f78170957884d3827
Adam Draper <adam@Adams-MacBook-Pro-2.local>
2016-12-01 23:54:15 -0800

    time format

---
+++ src/formats/time.js
@@ -0,0 +1,28 @@
+/*
+ * numeral.js format configuration
+ * format : time
+ * author : Adam Draper : https://github.com/adamwdraper
+ */
+(function () {
+    var numeral;
+
+    // get numeral from environment
+    if (typeof window !== 'undefined' && this.numeral) {
+        // Browser
+        numeral = this.numeral;
+    } else if (typeof module !== 'undefined' && module.exports) {
+        // Node
+        numeral = require('../numeral');
+    }
+
+    numeral.register('format', 'time', {
+        regexp: /(:)/,
+        format: function(value, format, roundingFunction) {
+            var hours = Math.floor(value / 60 / 60),
+                minutes = Math.floor((value - (hours * 60 * 60)) / 60),
+                seconds = Math.round(value - (hours * 60 * 60) - (minutes * 60));
+
+            return hours + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':' + (seconds < 10 ? '0' + seconds : seconds);
+        }
+    });
+}());
---
+++ tests/formats/time.js
@@ -0,0 +1,28 @@
+
+// Node
+if (typeof module !== 'undefined' && module.exports) {
+    var numeral = require('../../src/numeral');
+    var format = require('../../src/formats/time');
+    var expect = require('chai').expect;
+}
+
+describe('Time', function() {
+    after(function() {
+        numeral.reset();
+    });
+
+    it('should format to time', function() {
+        var tests = [
+                [0,'00:00:00','0:00:00'],
+                [null,'00:00:00','0:00:00'],
+                [25,'00:00:00','0:00:25'],
+                [238,'00:00:00','0:03:58'],
+                [63846,'00:00:00','17:44:06']
+            ],
+            i;
+
+        for (i = 0; i < tests.length; i++) {
+            expect(numeral(tests[i][0]).format(tests[i][1])).to.equal(tests[i][2]);
+        }
+    });
+});
