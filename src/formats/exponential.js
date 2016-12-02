9e5e8fb82611ff41473a3832a219b6280345bdda
Adam Draper <adam@Adams-MacBook-Pro-2.local>
2016-12-01 23:49:58 -0800

    exponential format

---
+++ src/formats/exponential.js
@@ -0,0 +1,32 @@
+/*
+ * numeral.js format configuration
+ * format : exponential
+ * author : Adam Draper : https://github.com/adamwdraper
+ */
+(function () {
+    var numeral;
+
+    // get numeral from environment
+    if (typeof window !== 'undefined' && this.numeral) {
+        // Browser
+        numeral = this.numeral;
+    } else if (typeof module !== 'undefined' && module.exports) {
+        // Node
+        numeral = require('../numeral');
+    }
+
+    numeral.register('format', 'exponential', {
+        regexp: /(e\+|e-)/,
+        format: function(value, format, roundingFunction) {
+            var output,
+                exponential = typeof value === 'number' && !numeral._.isNaN(value) ? value.toExponential() : '0e+0',
+                parts = exponential.split('e');
+
+            format = format.replace(/e[\+|\-]{1}0/, '');
+
+            output = numeral._.formatNumber(Number(parts[0]), format, roundingFunction);
+
+            return output + 'e' + parts[1];
+        }
+    });
+}());
--- tests/numeral/format.js
+++ tests/numeral/format.js
@@ -28,27 +28,6 @@ describe('Format', function() {
         });
     });
 
-    describe('Exponential', function() {
-        it('should format to exponential notation', function() {
-            var tests = [
-                    [0,'0e+0','0e+0'],
-                    [null,'0e+0','0e+0'],
-                    [1,'0e+0','1e+0'],
-                    [77.1234,'0.0e+0','7.7e+1'],
-                    [0.000000771234,'0.0e-0','7.7e-7'],
-                    [-0.000000771234,'0.00e-0','-7.71e-7'],
-                    [77.1234,'0.000e+0','7.712e+1'],
-                    [-1000830298,'0.0[000]e+0','-1.0008e+9']
-                ],
-                i;
-
-            for (i = 0; i < tests.length; i++) {
-                expect(numeral(tests[i][0]).format(tests[i][1])).to.equal(tests[i][2]);
-            }
-
-        });
-    });
-
     describe('Rounding', function() {
         it('should format with rounding', function() {
             var tests = [
