ec862cd2842fefc5effd8abb895f9c65b23e87e0
Patrick Arminio <patrick.arminio@gmail.com>
2016-12-22 14:26:05 +0000

    Allow register to override

--- src/numeral.js
+++ src/numeral.js
@@ -452,10 +452,10 @@
         options.defaultFormat = typeof(format) === 'string' ? format : '0.0';
     };
 
-    numeral.register = function(type, name, format) {
+    numeral.register = function(type, name, format, override) {
         name = name.toLowerCase();
 
-        if (this[type + 's'][name]) {
+        if (!override && this[type + 's'][name]) {
             throw new TypeError(name + ' ' + type + ' already registered.');
         }
 
