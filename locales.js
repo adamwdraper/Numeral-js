d986e165598742897f2c69237eaa94562dc20118
Marco Perrando <marco.perrando@gmail.com>
2017-02-03 11:35:01 +0100

    Fix: Italy uses the long scale.
    https://en.wikipedia.org/wiki/Long_and_short_scales#Other_long_scale

--- locales.js
+++ locales.js
@@ -453,8 +453,8 @@
         abbreviations: {
             thousand: 'mila',
             million: 'mil',
-            billion: 'b',
-            trillion: 't'
+            billion: 'mld',
+            trillion: 'bil'
         },
         ordinal: function (number) {
             return 'º';
