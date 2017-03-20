3a832fbc838f9cb496c20beaf11542e4c0abd0a6
Hoa Nguyen Thai <hoa.nguyen-thai@lazada.com>
2017-03-20 10:37:20 +0700

    add Vietnam locale (vi)

--- locales.js
+++ locales.js
@@ -820,4 +820,27 @@
 })();
 
 
+(function() {
+    
+    numeral.register('locale', 'vi', {
+        delimiters: {
+            thousands: '.',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: ' nghìn',
+            million: ' triệu',
+            billion: ' tỷ',
+            trillion: ' nghìn tỷ'
+        },
+        ordinal: function () {
+            return '.';
+        },
+        currency: {
+            symbol: '₫'
+        }
+    });
+})();
+
+
 }));
