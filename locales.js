b4cf7082a32ee5e9544f19422d57c42a6c1b4c4d
Neo <kqthang1505@gmail.com>
2017-03-26 12:24:34 +0700

    Add Locale for Vietnam number format. Add test also

--- locales.js
+++ locales.js
@@ -820,4 +820,26 @@
 })();
 
 
+(function() {
+        numeral.register('locale', 'vi', {
+        delimiters: {
+            thousands: '.',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'nghìn',
+            million: 'triệu',
+            billion: 'tỷ',
+            trillion: 'nghìn tỷ'
+        },
+        ordinal: function () {
+            return '';
+        },
+        currency: {
+            symbol: 'đ'
+        }
+    });
+})();
+
+
 }));
