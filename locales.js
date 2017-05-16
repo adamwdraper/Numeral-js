2a09431f9c70c07367d84d932336929ae94343a5
Patrick Eriksson <paleite@kth.se>
2017-05-16 17:46:05 +0200

    Added Swedish locale and test

--- locales.js
+++ locales.js
@@ -716,6 +716,29 @@
 
 
 (function() {
+        numeral.register('locale', 'sv', {
+        delimiters: {
+            thousands: ' ',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'm',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function (number) {
+            var b = number % 10;
+            return (b === 1 || b === 2) ? ':a' : ':e';
+        },
+        currency: {
+            symbol: 'kr'
+        }
+    });
+})();
+
+
+(function() {
     
 
     numeral.register('locale', 'th', {
---
+++ locales/sv.js
@@ -0,0 +1,33 @@
+// numeral.js locale configuration
+// locale : swedish
+// author : Patrick Eriksson
+
+(function (global, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['../numeral'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        factory(require('../numeral'));
+    } else {
+        factory(global.numeral);
+    }
+}(this, function (numeral) {
+    numeral.register('locale', 'sv', {
+        delimiters: {
+            thousands: ' ',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'm',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function (number) {
+            var b = number % 10;
+            return (b === 1 || b === 2) ? ':a' : ':e';
+        },
+        currency: {
+            symbol: 'kr'
+        }
+    });
+}));
