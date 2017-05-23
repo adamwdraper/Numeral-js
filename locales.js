79216dcc78df51dbe64570f9e81b3595581ade15
Stefano Ricciardi <sricciardi@opentable.com>
2017-05-23 17:55:34 +0100

    Added es-mx locale

--- locales.js
+++ locales.js
@@ -262,6 +262,33 @@
 
 
 (function() {
+        numeral.register('locale', 'es-mx', {
+        delimiters: {
+            thousands: ',',
+            decimal: '.'
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'M',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function (number) {
+            var b = number % 10;
+            return (b === 1 || b === 3) ? 'er' :
+                (b === 2) ? 'do' :
+                    (b === 7 || b === 0) ? 'mo' :
+                        (b === 8) ? 'vo' :
+                            (b === 9) ? 'no' : 'to';
+        },
+        currency: {
+            symbol: '$'
+        }
+    });
+})();
+
+
+(function() {
         numeral.register('locale', 'es', {
         delimiters: {
             thousands: '.',
---
+++ locales/es-mx.js
@@ -0,0 +1,37 @@
+// numeral.js locale configuration
+// locale : spanish Mexico
+// author : Stefano Ricciardi: https://www.stefanoricciardi.com
+
+(function (global, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['../numeral'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        factory(require('../numeral'));
+    } else {
+        factory(global.numeral);
+    }
+}(this, function (numeral) {
+    numeral.register('locale', 'es-mx', {
+        delimiters: {
+            thousands: ',',
+            decimal: '.'
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'M',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function (number) {
+            var b = number % 10;
+            return (b === 1 || b === 3) ? 'er' :
+                (b === 2) ? 'do' :
+                    (b === 7 || b === 0) ? 'mo' :
+                        (b === 8) ? 'vo' :
+                            (b === 9) ? 'no' : 'to';
+        },
+        currency: {
+            symbol: '$'
+        }
+    });
+}));
