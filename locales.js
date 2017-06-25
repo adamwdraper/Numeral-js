bba1e9f08e4f2ca1f6731d66223a7eca3a42e7ac
Anwar Sarmiento Ramos <anwarsarmiento@gmail.com>
2017-06-25 13:28:56 -0600

    Simbolo de Costa Rica
    Simbolo de Colones

--- locales.js
+++ locales.js
@@ -842,5 +842,30 @@
     });
 })();
 
+(function() {
+        numeral.register('locale', 'cr', {
+        delimiters: {
+            thousands: ',',
+            decimal: '.'
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'mm',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function (number) {
+            var b = number % 10;
+            return (b === 1 || b === 3) ? 'er' :
+                (b === 2) ? 'do' :
+                (b === 7 || b === 0) ? 'mo' :
+		(b === 8) ? 'vo' :
+		(b === 9) ? 'no' : 'to';
+        },
+        currency: {
+            symbol: '₡'
+        }
+    });
+})();	
 
 }));
