1972524429905bfd4986a61b76d2e1af9285dad8
Mattias Halldin <mattias.halldin@gmail.com>
2017-07-04 12:47:47 +0200

    Update sv-se.js

--- locales/sv-se.js
+++ locales/sv-se.js
@@ -14,22 +14,20 @@
     numeral.register('locale', 'sv-se', {
         delimiters: {
             thousands: ' ',
-            decimal: ',',
+            decimal: ','
         },
         abbreviations: {
             thousand: 'k',
             million: 'm',
             billion: 'md',
-            trillion: 'bn',
+            trillion: 'bn'
         },
         ordinal: function (number) {
             var b = number % 10;
-            return ~~(number % 100 / 10) === 1
-              ? ':a'
-              : b === 1 ? ':a' : b === 2 ? ':a' : b === 3 ? ':e' : ':e';
+            return ~~(number % 100 / 10) === 1 ? ':a' : b === 1 ? ':a' : b === 2 ? ':a' : b === 3 ? ':e' : ':e';
         },
         currency: {
-          symbol: 'SKR',
+          symbol: 'SKR'
         }
     });
 }));
