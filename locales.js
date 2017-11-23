b2c6575027c53341fbbb4e32180f3ab740f48bc7
Blas Pico <toni.pico@gmail.com>
2017-11-23 19:08:36 -0300

    Locale Paraguay

--- locales.js
+++ locales.js
@@ -262,6 +262,33 @@
 
 
 (function() {
+        numeral.register('locale', 'es-py', {
+        delimiters: {
+            thousands: '.',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'mm',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal: function (number) {
+            var b = number % 10;
+            return (b === 1 || b === 3) ? 'er' :
+                (b === 2) ? 'do' :
+                    (b === 7 || b === 0) ? 'mo' :
+                        (b === 8) ? 'vo' :
+                            (b === 9) ? 'no' : 'to';
+        },
+        currency: {
+            symbol: '₲'
+        }
+    });
+})();
+
+
+(function() {
         numeral.register('locale', 'es', {
         delimiters: {
             thousands: '.',
