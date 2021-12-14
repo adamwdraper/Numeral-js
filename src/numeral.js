8ce39bd6d89edf982795c3e055361b0b305754c8
Seyyed Parsa Neshaei <spneshaei@users.noreply.github.com>
2021-12-14 21:45:54 +0330

    Fixed issue #600

--- src/numeral.js
+++ src/numeral.js
@@ -659,6 +659,20 @@
         },
         difference: function(value) {
             return Math.abs(numeral(this._value).subtract(value).value());
+        },
+        roman: function(value) {
+            if (isNaN(value)) {
+                return NaN;
+            }
+            var digits = String(+value).split(""),
+                key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+                    "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+                    "","I","II","III","IV","V","VI","VII","VIII","IX"],
+                roman = "",
+                i = 3;
+            while (i--)
+                roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+            return Array(+digits.join("") + 1).join("M") + roman;
         }
     };
 
